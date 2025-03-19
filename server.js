8b49fb3344617c03df6b18952bccd89475f51a45
Varun Patel <varunpatel@varuns-mbp-2.lan>
2025-03-19 16:18:48 -0400

    host server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const cors = require("cors");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // cors stuff
 app.use(cors());
